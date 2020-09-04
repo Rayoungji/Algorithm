eeb41f0dcf986306ff5860e71818d0d95137fb79
rayoungji <dudwl7676@naver.com>
2020-09-04 17:42:00 +0900

    주석 제거

--- src/beakjun/sort/BJ_10989_수정렬하기3.java
+++ src/beakjun/sort/BJ_10989_수정렬하기3.java
@@ -4,10 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-/*
-여러개의 출력을 요구할 때 for문을 돌려 모두 sout 하지말고 StringBuilder에 저장 후 한번에 sout 하면 시간절약!!
-br.apped : String의 + 연산자와 같은 역할을 함
-* */
 public class BJ_10989_수정렬하기3 {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
