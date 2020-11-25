8cfd745e8b26ff212755a7aca066174d581618c1
rayoungji <dudwl7676@naver.com>
2020-11-25 11:36:49 +0900

    PG_다리를 지나는 트럭

---
+++ src/Programmers/Queue/PG_다리를_지나는_트럭.java
@@ -0,0 +1,43 @@
+package Programmers.Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class PG_다리를_지나는_트럭 {
+    public static void main(String args[]) {
+        int[] truck = {7, 4, 5, 1, 6};
+        int a = solution(2, 10, truck);
+        System.out.println(a);
+    }
+
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer>queue=new LinkedList<>();
+        int sum=0;
+        int answer=0;
+
+        for(int i : truck_weights) {
+            while (true) {
+                if(queue.isEmpty()) {
+                    queue.add(i);
+                    sum = sum+i;
+                    answer++;
+                    break;
+                } else if (queue.size() == bridge_length) {
+                    sum=sum-queue.poll();
+                } else {
+                    if(sum+i > weight) {
+                        queue.add(0);
+                        answer++;
+                    } else {
+                        queue.add(i);
+                        sum= sum+i;
+                        answer++;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer+bridge_length;
+    }
+}
