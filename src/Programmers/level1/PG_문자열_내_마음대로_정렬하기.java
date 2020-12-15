d2d3a8d5a425e22f253f1ce5390e3063b13c0f1e
rayoungji <dudwl7676@naver.com>
2020-12-15 19:19:14 +0900

    문자열 내 마음대로 정렬하기

---
+++ src/Programmers/level1/PG_문자열_내_마음대로_정렬하기.java
@@ -0,0 +1,28 @@
+package Programmers.level1;
+
+import java.util.*;
+
+public class PG_문자열_내_마음대로_정렬하기 {
+    public static void main(String args[]){
+        String []strings = {"sun", "bed", "car"};
+        String answer[] = solution(strings,1);
+        for (String s : answer) {
+            System.out.println(s);
+        }
+    }
+
+    public static String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.charAt(n) > o2.charAt(n))
+                    return 1;
+                else if (o1.charAt(n) == o2.charAt(n))
+                    return o1.compareTo(o2);
+                else
+                    return -1;
+            }
+        });
+        return strings;
+    }
+}
