7af80a1490f815ae23118be9cff7b4cf648ab0fc
rayoungji <dudwl7676@naver.com>
2020-12-18 16:08:19 +0900

    나누어 떨어지는 숫자배열

---
+++ src/Programmers/level1/PG_나누어_떨어지는_숫자_배열.java
@@ -0,0 +1,36 @@
+package Programmers.level1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PG_나누어_떨어지는_숫자_배열 {
+    public static void main(String args[]) {
+        int array[] = {5, 9, 7, 10};
+        System.out.println(Arrays.toString(solution(array, 10)));
+    }
+
+    public static int[] solution(int[] arr, int divisor) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i : arr) {
+            if (i % divisor == 0) {
+                list.add(i);
+            }
+        }
+
+        Collections.sort(list);
+        if (list.isEmpty()) {
+            int answer2[] = new int[1];
+            answer2[0] = -1;
+            return answer2;
+        } else {
+            int answer[] = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                answer[i] = list.get(i);
+            }
+            return answer;
+        }
+    }
+}
