08d32d289319caddb1aa3074297652542daecabf
rayoungji <dudwl7676@naver.com>
2020-12-19 19:51:58 +0900

    문자열 내 p/y 갯수 구하기 - 스트림 사용하기

--- src/Programmers/level1/PG_문재열내_p와y의_갯수.java
+++ src/Programmers/level1/PG_문재열내_p와y의_갯수.java
@@ -4,17 +4,19 @@ public class PG_문재열내_p와y의_갯수 {
     public static void main(String args[]) {
         String s = "pyY";
         System.out.println(solution(s));
+        System.out.println(solutionByStream(s));
     }
 
     public static boolean solution(String s) {
+        s = s.toUpperCase();
         char chars[] = s.toCharArray();
         int pNum = 0;
         int yNum = 0;
         for (char i : chars) {
-            if (i == 'p' || i == 'P') {
+            if (i == 'P') {
                 pNum++;
             }
-            if (i == 'y' || i == 'Y') {
+            if (i == 'Y') {
                 yNum++;
             }
         }
@@ -23,5 +25,10 @@ public class PG_문재열내_p와y의_갯수 {
         } else {
             return false;
         }
+    }
+
+    public static boolean solutionByStream(String s) {
+        s = s.toUpperCase();
+        return s.chars().filter(i -> 'P' == i).count() == s.chars().filter(i -> 'Y' == i).count();
     }
 }
