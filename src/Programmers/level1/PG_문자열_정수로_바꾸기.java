755daa9422dcfaf3477fa17e5ccc3f4768fb149d
rayoungji <dudwl7676@naver.com>
2020-12-20 13:25:51 +0900

    문자열 정수로 바꾸기

---
+++ src/Programmers/level1/PG_문자열_정수로_바꾸기.java
@@ -0,0 +1,10 @@
+package Programmers.level1;
+
+public class PG_문자열_정수로_바꾸기 {
+    public static void main(String args[]){
+        System.out.println(solution("-1234"));
+    }
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
