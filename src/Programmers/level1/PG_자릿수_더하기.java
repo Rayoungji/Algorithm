73847063f62eb14fe6c2f792d20ff972edb5215b
rayoungji <dudwl7676@naver.com>
2021-01-01 22:59:12 +0900

    자릿수 더하기

--- src/Programmers/level1/PG_자릿수_더하기.java
+++ src/Programmers/level1/PG_자릿수_더하기.java
@@ -3,6 +3,7 @@ package Programmers.level1;
 public class PG_자릿수_더하기 {
     public static void main(String args[]){
         System.out.println(solution(987));
+        System.out.println(solution2(987));
     }
     public static int solution(int n) {
         int answer = 0;
@@ -13,6 +14,15 @@ public class PG_자릿수_더하기 {
             answer +=Integer.parseInt(String.valueOf(s.charAt(i)));
         }
 
+        return answer;
+    }
+    public static int solution2(int n) {
+        int answer = 0;
+
+        while(n>0) {
+            answer += n%10;
+            n = n/10;
+        }
         return answer;
     }
 }
