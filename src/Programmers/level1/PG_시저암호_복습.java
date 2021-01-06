f85908fd30f1c81ce18fbe15874daf5b4d75fa47
rayoungji <dudwl7676@naver.com>
2021-01-06 09:53:15 +0900

    시저암호 코드 수정

--- src/Programmers/level1/PG_시저암호_복습.java
+++ src/Programmers/level1/PG_시저암호_복습.java
@@ -3,39 +3,23 @@ package Programmers.level1;
 
 public class PG_시저암호_복습 {
     public static void main(String args[]) {
-
-        System.out.println(solution("xxXy",23));
-        System.out.println((int)'x' + 23);
-        System.out.println((int)'z');
-        System.out.println((int)'Z');
-
+        System.out.println(solution("a a a", 3));
     }
-    public static String solution(String s, int n) {
-        char[] array = s.toCharArray();
-        String answer ="";
-        for (char i : array) {
-            if (i==' ') {
-                answer+=" ";
-            }
-            else if((int)i+n < (int)'Z' || (int)i+n <(int)'z') {
-                answer +=String.valueOf((char)((int)i + n));
-            }
-            else if ((int)i == (int)'z') {
-                char a = 'a';
-                answer += (String.valueOf((char)((int)a+n-1)));
 
-            }
-            else if ((int)i == (int)'Z') {
-                char A = 'A';
-                answer += (String.valueOf((char)((int)A+n-1)));
-            }
-            else if ((int)i+n > (int)'Z') {
-                answer += (String.valueOf((char)((int)'A' + ((int)'Z' - (int)i -1))));
-            }
-            else if ((int)i+n > (int)'z') {
-                answer += (String.valueOf((char)((int)'a' + ((int)'z' - (int)i -1))));
+    public static String solution(String s, int n) {
+        char[] chars = s.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char i : chars) {
+            if (Character.isLowerCase(i)) {
+                char an = i + n > 'z' ? (char) ('a' + (i + n - 'z' - 1)) : (char) (i + n);
+                stringBuilder.append(an);
+            } else if (Character.isUpperCase(i)) {
+                char an = i + n > 'Z' ? (char) ('A' + (i + n - 'Z' - 1)) : (char) (i + n);
+                stringBuilder.append(an);
+            } else {
+                stringBuilder.append(' ');
             }
         }
-        return answer;
+        return stringBuilder.toString();
     }
 }
