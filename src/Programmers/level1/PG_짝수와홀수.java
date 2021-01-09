ee514a8e689a935cc22360ab1f397db42bf129d0
rayoungji <dudwl7676@naver.com>
2021-01-09 23:37:30 +0900

    짝수와 홀수

---
+++ src/Programmers/level1/PG_짝수와홀수.java
@@ -0,0 +1,16 @@
+package Programmers.level1;
+
+public class PG_짝수와홀수 {
+    public static void main(String args[]) {
+
+    }
+
+    public static String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+
+    }
+}
