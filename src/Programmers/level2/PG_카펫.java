a3f3d717092785c2a914f01e45ebabf532b74049
rayoungji <dudwl7676@naver.com>
2021-01-29 16:42:45 +0900

    카펫

---
+++ src/Programmers/level2/PG_카펫.java
@@ -0,0 +1,28 @@
+package Programmers.level2;
+
+public class PG_카펫 {
+
+    public static void main(String args[]){
+
+    }
+    public int[] solution(int brown, int red) {
+
+        int height = 0;
+        int width = 0;
+        for (height = 3; height <= (int) (brown + 4) / 2; height++) {
+
+            width = ((brown + 4) / 2) - height;
+            if (width < height) {
+                break;
+            }
+
+            int redCount = (width - 2) * (height - 2);
+            if (red == redCount) {
+                break;
+            }
+        }
+
+        int[] answer = new int[] { width, height };
+        return answer;
+    }
+}
